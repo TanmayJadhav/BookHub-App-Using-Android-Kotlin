eb3c9434d9bc83ba256c6a3ca8151192d73becdf
Tanmay Jadhav <tanmayjadhavtj11@gmail.com>
2021-02-04 21:05:01 +0530

    Book Data Added

--- app/src/main/java/com/bookhub/adapter/DashboardRecyclerAdapter.kt
+++ app/src/main/java/com/bookhub/adapter/DashboardRecyclerAdapter.kt
@@ -4,15 +4,24 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.bookhub.R
+import com.bookhub.model.Book
 
-class DashboardRecyclerAdapter(val context: Context ,val itemList:ArrayList<String>) : RecyclerView.Adapter<DashboardRecyclerAdapter.DashboardViewHolder>() {
+class DashboardRecyclerAdapter(val context: Context ,val itemList:ArrayList<Book>) : RecyclerView.Adapter<DashboardRecyclerAdapter.DashboardViewHolder>() {
 
     class DashboardViewHolder(view:View) : RecyclerView.ViewHolder(view)
     {
-        val textView : TextView = view.findViewById(R.id.txtBookName)
+        val bookName : TextView = view.findViewById(R.id.txtBookName)
+        val bookAuthor : TextView = view.findViewById(R.id.book_author)
+        val bookRating : TextView = view.findViewById(R.id.rating)
+        val bookPrice : TextView = view.findViewById(R.id.book_price)
+        val bookImage : ImageView = view.findViewById(R.id.book_image)
+        val book_click_listner : ConstraintLayout = view.findViewById(R.id.book_click)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DashboardViewHolder {
@@ -22,8 +31,18 @@ class DashboardRecyclerAdapter(val context: Context ,val itemList:ArrayList<Stri
     }
 
     override fun onBindViewHolder(holder: DashboardViewHolder, position: Int) {
-        val text = itemList[position]
-        holder.textView.text = text
+
+        val book = itemList[position]
+        holder.bookName.text = book.bookName
+        holder.bookAuthor.text = book.bookAuthor
+        holder.bookPrice.text = book.bookCost
+        holder.bookRating.text = book.bookRating
+        holder.bookImage.setBackgroundResource(book.bookImage)
+
+        holder.book_click_listner.setOnClickListener{
+            Toast.makeText(context,"Clicked on ${holder.bookName.text}",Toast.LENGTH_LONG).show()
+        }
+
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/bookhub/fragment/DashboardFragment.kt
+++ app/src/main/java/com/bookhub/fragment/DashboardFragment.kt
@@ -2,6 +2,7 @@ package com.bookhub.fragment
 
 
 import android.content.Context
+import android.os.Build.VERSION_CODES.P
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,8 +10,10 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
 import com.bookhub.R
 import com.bookhub.adapter.DashboardRecyclerAdapter
+import com.bookhub.model.Book
 
 class  DashboardFragment : Fragment() {
 
@@ -19,13 +22,23 @@ class  DashboardFragment : Fragment() {
     lateinit var layoutManager: RecyclerView.LayoutManager
 
     val bookList = arrayListOf(
-        "P.S  I Love You",
+        "P.S I Love You",
         "The Gray Gatsby",
         "Anna Karenina",
         "Madam Bovary",
         "War and Peace",
         "The Alchemist",
         "Lord of the Rings"
+    )
+
+    val bookInfoList = arrayListOf<Book>(
+            Book("P.S I Love You","Cecelia Ahren","Rs. 299","4.5",R.drawable.ps_i_love_you),
+            Book("The Gray Gatsby","F. Scott Fitzgerland","Rs. 399","4.3",R.drawable.the_gray_gatsby),
+            Book("Anna Karenina","Leo Tolstoy","Rs. 199","4.4",R.drawable.anna_karenina),
+            Book("Madam Bovary","Gustave Fluabert","Rs. 500","4.8",R.drawable.madam_bovary),
+            Book("War and Peace","Leo Tolstoy","Rs. 249","4.2",R.drawable.war_and_peace),
+            Book("Alchemist","Paulo Coelho","Rs. 349","4.9",R.drawable.alchemist),
+            Book("Lord of the Rings","J.R.R Tolkein","Rs. 799","5.0",R.drawable.lord_of_the_rings)
     )
 
     lateinit var recyclerAdapter: DashboardRecyclerAdapter
@@ -41,11 +54,18 @@ class  DashboardFragment : Fragment() {
 
         layoutManager = LinearLayoutManager(activity);
 
-        recyclerAdapter = DashboardRecyclerAdapter(activity as Context , bookList)
+        recyclerAdapter = DashboardRecyclerAdapter(activity as Context , bookInfoList)
 
         recyclerDashboard.adapter = recyclerAdapter
 
         recyclerDashboard.layoutManager = layoutManager
+
+        recyclerDashboard.addItemDecoration(
+                DividerItemDecoration(
+                        recyclerDashboard.context,
+                        (layoutManager as LinearLayoutManager).orientation
+                )
+        )
 
         return view
     }
